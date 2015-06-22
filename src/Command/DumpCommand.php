96b6dbcb37b1b0bc586c7e32b4125313979ef664
Christoph Nißle <christoph.nissle@enterio.de>
2015-06-22 17:37:01 +0200

    first draft for creating a db connection

--- src/Command/DumpCommand.php
+++ src/Command/DumpCommand.php
@@ -2,6 +2,8 @@
 
 namespace Spraed\DumpThis\Command;
 
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -16,14 +18,14 @@ use Symfony\Component\Yaml\Yaml;
 class DumpCommand extends Command
 {
     /**
-     *
+     * Defines cli command and its options
      */
     protected function configure()
     {
         $this->setName('spraed:dump')
             ->setDescription('Get MSSQL tables and its content, write to MySQL database')
-            ->addOption('source', null, InputOption::VALUE_REQUIRED, 'Connection to pull from')
-            ->addOption('goal', null, InputOption::VALUE_REQUIRED, 'Connection to dump to');
+            ->addOption('source', null, InputOption::VALUE_OPTIONAL, 'Connection to pull from', 'mssql')
+            ->addOption('goal', null, InputOption::VALUE_OPTIONAL, 'Connection to dump to', 'mysql');
     }
 
     /**
@@ -37,22 +39,34 @@ class DumpCommand extends Command
         $source = $input->getOption('source');
         $goal = $input->getOption('goal');
 
-
         // connect to mssql
-
+        $sourceConnection = $this->getConnection($source);
         // get tablenames
         // get column names per table
         // get data
 
         // connect to mysql
+        $goalConnection = $this->getConnection($source);
         // write tablenames
         // write column names per table
         // write data
     }
 
+    /**
+     * @param $config
+     *
+     * @return \Doctrine\DBAL\Connection
+     */
     private function getConnection($config)
     {
-        $array = Yaml::parse(file_get_contents(__DIR__ .'../Resources/config/config.yml'));
+        $array = Yaml::parse(file_get_contents(__DIR__ . '../Resources/config/config.yml'));
+
+        $connectionParams = $array['doctrine']['dbal']['connections'][$config];
+
+        $config = new Configuration();
+        $connection = DriverManager::getConnection($connectionParams, $config);
+
+        return $connection;
     }
 }
  
